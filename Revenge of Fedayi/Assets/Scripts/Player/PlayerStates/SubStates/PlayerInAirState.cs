9e651c4a68825c3248f1404732eaf5780c85ef58
Marlen Zhumakov <mzhumakov@gmail.com>
2023-05-18 17:55:08 +0600

    small fix of wall jumping in air state

--- Revenge of Fedayi/Assets/Scripts/Player/PlayerStates/SubStates/PlayerInAirState.cs
+++ Revenge of Fedayi/Assets/Scripts/Player/PlayerStates/SubStates/PlayerInAirState.cs
@@ -56,6 +56,11 @@ public class PlayerInAirState : PlayerState
     public override void Exit()
     {
         base.Exit();
+
+        oldIsTouchingWall = false;
+        oldIsTouchingWallBack = false;
+        isTouchingWall = false;
+        isTouchingWallBack = false;
     }
 
     public override void LogicUpdate()
